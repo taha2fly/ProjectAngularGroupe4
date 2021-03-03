0f1a4648b967ed07af29bf1a68f272dbfac0f566
taha2fly <taha.elrobrini@gmail.com>
2021-03-03 23:24:30 +0100

    Adding text entry areas in the display that allow us to modify a property of the hard coded capsule/launch. The changes are reflected immediatly. We used FormsModule to achieve this feature

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; 
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,16 +8,17 @@ import { LaunchesComponent } from './launches/launches/launches.component';
 import { CapsulesComponent } from './capsules/capsules/capsules.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LaunchesComponent,
-    CapsulesComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		LaunchesComponent,
+		CapsulesComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		FormsModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
